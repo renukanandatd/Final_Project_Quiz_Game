220f0690c41101114ce194a230bcdb18422ad270
renukanandatd <renukananda2000@gmail.com>
2023-10-05 16:26:52 +0530

    Sign Up screen added

--- app/src/main/java/com/example/final_project_quiz_game/LoginActivity.kt
+++ app/src/main/java/com/example/final_project_quiz_game/LoginActivity.kt
@@ -1,11 +1,32 @@
 package com.example.final_project_quiz_game
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.final_project_quiz_game.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var loginBinding:ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        loginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = loginBinding.root
+        setContentView(view)
+
+        loginBinding.buttonSignIn.setOnClickListener {
+
+        }
+        loginBinding.googleSignIn.setOnClickListener {
+
+        }
+        loginBinding.textViewSignUp.setOnClickListener {
+            val intent = Intent(this@LoginActivity,Sign_Up_Activity::class.java)
+            startActivity(intent)
+        }
+        loginBinding.textViewForgotPassword.setOnClickListener {
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/final_project_quiz_game/Sign_Up_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.final_project_quiz_game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sign_Up_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
