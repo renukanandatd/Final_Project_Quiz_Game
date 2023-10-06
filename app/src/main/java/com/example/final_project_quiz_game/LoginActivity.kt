6aac3fa3d69c1a1ce712471a3f9a56523f1ba0a6
renukanandatd <renukananda2000@gmail.com>
2023-10-06 12:24:16 +0530

    Firebase authentication added

--- app/src/main/java/com/example/final_project_quiz_game/LoginActivity.kt
+++ app/src/main/java/com/example/final_project_quiz_game/LoginActivity.kt
@@ -4,10 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.final_project_quiz_game.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.identity.SignInPassword
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var loginBinding:ActivityLoginBinding
+    val auth : FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +19,10 @@ class LoginActivity : AppCompatActivity() {
         setContentView(view)
 
         loginBinding.buttonSignIn.setOnClickListener {
+
+            val email = loginBinding.textEmail.text.toString()
+            val password = loginBinding.textPassword.text.toString()
+            loginWithEmail(email,password)
 
         }
         loginBinding.googleSignIn.setOnClickListener {
@@ -28,5 +35,10 @@ class LoginActivity : AppCompatActivity() {
         loginBinding.textViewForgotPassword.setOnClickListener {
 
         }
+    }
+
+    fun loginWithEmail(email: String,password: String){
+
+
     }
 }
--- app/src/main/java/com/example/final_project_quiz_game/Sign_Up_Activity.kt
+++ app/src/main/java/com/example/final_project_quiz_game/Sign_Up_Activity.kt
@@ -2,10 +2,48 @@ package com.example.final_project_quiz_game
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.final_project_quiz_game.databinding.ActivityMainBinding
+import com.example.final_project_quiz_game.databinding.ActivitySignUpBinding
+import com.google.android.gms.auth.api.identity.SignInPassword
+import com.google.firebase.auth.FirebaseAuth
 
 class Sign_Up_Activity : AppCompatActivity() {
+
+    lateinit var signUpBinding: ActivitySignUpBinding
+    val auth : FirebaseAuth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        signUpBinding = ActivitySignUpBinding.inflate(layoutInflater)
+        val view = signUpBinding.root
+        setContentView(view)
+
+        signUpBinding.buttonSignUp.setOnClickListener {
+
+            val email = signUpBinding.textSignUpEmail.text.toString()
+            val password = signUpBinding.textSignUpPassword.text.toString()
+            signUpWithEmail(email,password)
+        }
+    }
+
+    fun signUpWithEmail(email:String,password:String){
+
+        signUpBinding.progressBarSignUp.visibility = View.VISIBLE
+        signUpBinding.buttonSignUp.isClickable = false
+
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+
+            if(task.isSuccessful){
+                Toast.makeText(applicationContext,"Your account has been created succesfully",Toast.LENGTH_LONG).show()
+                finish()
+                signUpBinding.progressBarSignUp.visibility = View.INVISIBLE
+                signUpBinding.buttonSignUp.isClickable = true
+
+            }else{
+                Toast.makeText(applicationContext,task.exception?.localizedMessage,Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 }
