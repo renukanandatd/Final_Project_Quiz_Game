6a5e56ffdff6e6cd31a93ca3232d0b3714b6b3fc
renukanandatd <renukananda2000@gmail.com>
2023-10-10 00:29:11 +0530

    dialog box for ending game and also to send final score to firebase feature added

--- app/src/main/java/com/example/final_project_quiz_game/QuizActivity.kt
+++ app/src/main/java/com/example/final_project_quiz_game/QuizActivity.kt
@@ -1,12 +1,15 @@
 package com.example.final_project_quiz_game
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.final_project_quiz_game.databinding.ActivityQuizBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -37,6 +40,10 @@ class QuizActivity : AppCompatActivity() {
     var timerContinue = false
     var leftTime = totalTime
 
+    val auth = FirebaseAuth.getInstance()
+    val user = auth.currentUser
+    val scoreRef = database.reference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         quizBinding = ActivityQuizBinding.inflate(layoutInflater)
@@ -49,68 +56,93 @@ class QuizActivity : AppCompatActivity() {
             resetTimer()
             gameLogic()
         }
-        quizBinding.buttonFinish.setOnClickListener {
 
+        quizBinding.buttonFinish.setOnClickListener {
+            sendScore()
         }
+
         quizBinding.textViewOption1.setOnClickListener {
             pauseTimer()
             userAnswer = "a"
+
             if(userAnswer==correctAnswer){
                 quizBinding.textViewOption1.setBackgroundColor(Color.GREEN)
                 answerCorrect++
                 quizBinding.textViewCorrectAnswer.text = answerCorrect.toString()
-            }else{
+            }
+
+            else{
                 quizBinding.textViewOption1.setBackgroundColor(Color.RED)
                 answerWrong++
                 quizBinding.textViewWrongAnswer.text = answerWrong.toString()
                 findAnswer()
             }
+
             disableClickable()
+
         }
+
         quizBinding.textViewOption2.setOnClickListener {
             pauseTimer()
             userAnswer = "b"
+
             if(userAnswer==correctAnswer){
                 quizBinding.textViewOption2.setBackgroundColor(Color.GREEN)
                 answerCorrect++
                 quizBinding.textViewCorrectAnswer.text = answerCorrect.toString()
-            }else{
+            }
+
+            else{
                 quizBinding.textViewOption2.setBackgroundColor(Color.RED)
                 answerWrong++
                 quizBinding.textViewWrongAnswer.text = answerWrong.toString()
                 findAnswer()
             }
+
             disableClickable()
+
         }
+
         quizBinding.textViewOption3.setOnClickListener {
             pauseTimer()
             userAnswer = "c"
+
             if(userAnswer==correctAnswer){
                 quizBinding.textViewOption3.setBackgroundColor(Color.GREEN)
                 answerCorrect++
                 quizBinding.textViewCorrectAnswer.text = answerCorrect.toString()
-            }else{
+            }
+
+            else{
                 quizBinding.textViewOption3.setBackgroundColor(Color.RED)
                 answerWrong++
                 quizBinding.textViewWrongAnswer.text = answerWrong.toString()
                 findAnswer()
             }
+
             disableClickable()
+
         }
+
         quizBinding.textViewOption4.setOnClickListener {
             pauseTimer()
             userAnswer = "d"
+
             if(userAnswer==correctAnswer){
                 quizBinding.textViewOption4.setBackgroundColor(Color.GREEN)
                 answerCorrect++
                 quizBinding.textViewCorrectAnswer.text = answerCorrect.toString()
-            }else{
+            }
+
+            else{
                 quizBinding.textViewOption4.setBackgroundColor(Color.RED)
                 answerWrong++
                 quizBinding.textViewCorrectAnswer.text = answerWrong.toString()
                 findAnswer()
             }
+
             disableClickable()
+
         }
 
     }
@@ -146,10 +178,33 @@ class QuizActivity : AppCompatActivity() {
 
                     startTimer()
 
-                }else{
-                    Toast.makeText(applicationContext,"You answered all the questions",Toast.LENGTH_LONG).show()
                 }
+                else{
+
+                    val dialogMessage = AlertDialog.Builder(this@QuizActivity)
+                    dialogMessage.setTitle("Quia Game")
+                    dialogMessage.setMessage("Congratulations!!! \nYou have answered all the questions, do you want to see the results?")
+                    dialogMessage.setCancelable(false)
+                    dialogMessage.setPositiveButton("See Result"){dialogWindow,position ->
+
+                        sendScore()
+
+                    }
+
+                    dialogMessage.setNegativeButton("Play Again"){dialogWindow,position ->
+
+                        val intent = Intent(this@QuizActivity,MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+
+                    }
+
+                    dialogMessage.create().show()
+
+                }
+
                 questionNumber++
+
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -218,5 +273,20 @@ class QuizActivity : AppCompatActivity() {
     fun pauseTimer(){
         timer.cancel()
         timerContinue = false
+    }
+
+    fun sendScore(){
+
+        user?.let {
+            val userId = it.uid
+            scoreRef.child("scores").child(userId).child("correct").setValue(answerCorrect)
+            scoreRef.child("scores").child(userId).child("wrong").setValue(answerWrong).addOnSuccessListener {
+                Toast.makeText(applicationContext,"Scores sent to database successfully",Toast.LENGTH_LONG).show()
+                val intent = Intent(this@QuizActivity,ResultActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
     }
 }
