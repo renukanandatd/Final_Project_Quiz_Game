56bc14376e8463c945370d2d5f10d0c98f7f1d72
renukanandatd <renukananda2000@gmail.com>
2023-10-10 12:01:47 +0530

    final commit

--- app/src/main/java/com/example/final_project_quiz_game/ResultActivity.kt
+++ app/src/main/java/com/example/final_project_quiz_game/ResultActivity.kt
@@ -1,11 +1,68 @@
 package com.example.final_project_quiz_game
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.final_project_quiz_game.databinding.ActivityResultBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class ResultActivity : AppCompatActivity() {
+
+    lateinit var resultBinding: ActivityResultBinding
+
+    val database = FirebaseDatabase.getInstance()
+    val databaseReference = database.reference.child("scores")
+
+    val auth = FirebaseAuth.getInstance()
+    val user = auth.currentUser
+
+    var userCorrect = ""
+    var userWrong = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        resultBinding = ActivityResultBinding.inflate(layoutInflater)
+        val view = resultBinding.root
+        setContentView(view)
+
+        databaseReference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                user?.let {
+
+                    val userId = it.uid
+
+                    userCorrect = snapshot.child(userId).child("correct").value.toString()
+                    userWrong = snapshot.child(userId).child("wrong").value.toString()
+
+                    resultBinding.correctAnswer.text = userCorrect
+                    resultBinding.wrongAnswer.text = userWrong
+
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+        resultBinding.buttonPlayAgain.setOnClickListener {
+
+            val intent = Intent(this@ResultActivity,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+
+        resultBinding.buttonExit.setOnClickListener {
+
+            finish()
+
+        }
     }
 }
